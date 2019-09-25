d533dac78e6d405775e456638de0aeb0a0738c04
jaeseopark <20038316+jaeseopark@users.noreply.github.com>
2019-09-25 01:04:49 -0600

    process_video skeleton code

--- ytmdl/process_video.py
+++ ytmdl/process_video.py
@@ -1,2 +1,12 @@
-def process_video():
-    pass
+import json
+import logging
+
+from ytmdl import firestore
+
+LOGGER = logging.getLogger(__name__)
+
+
+def process_video(event, context=None):
+    LOGGER.info(json.dumps(event))  # removeme
+    handled_event = firestore.handle_event(event)
+    LOGGER.info(json.dumps(handled_event))  # removeme
