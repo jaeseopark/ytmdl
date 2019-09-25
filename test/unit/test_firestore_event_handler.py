c3aee862d06584a729ce14f0a96b5b29d53c37a9
jaeseopark <20038316+jaeseopark@users.noreply.github.com>
2019-09-25 00:01:25 -0600

    tests for firestore event handler

---
+++ test/unit/test_firestore_event_handler.py
@@ -0,0 +1,27 @@
+import json
+
+from ytmdl.firestore import handle_event
+
+
+def test_update_existing_field():
+    with open('test_data/firestore_event_update_existing_field.json') as fp:
+        event = json.load(fp)
+
+    handled_event = handle_event(event)
+
+    lt = handled_event["last_triggered"]
+    assert lt[0] == 1569387601
+    assert lt[1] == 1569389005
+    assert handled_event["id"] == "default"
+
+
+def test_update_new_field():
+    with open('test_data/firestore_event_update_new_field.json') as fp:
+        event = json.load(fp)
+
+    handled_event = handle_event(event)
+
+    lt = handled_event["last_triggered"]
+    assert lt[0] == None
+    assert lt[1] == 1569389560
+    assert handled_event["id"] == "default"
