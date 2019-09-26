f5a3014bb3c422df84dfef37690403739e1ea7e1
Jaeseo Park <jaeseopark@icloud.com>
2019-09-26 00:45:42 -0600

    add authenticate()

--- ytmdl/utils/gcp_utils.py
+++ ytmdl/utils/gcp_utils.py
@@ -4,14 +4,16 @@ import os
 import googleapiclient.discovery
 import googleapiclient.errors
 from firebase_admin import credentials
-from oauth2client import file
+from oauth2client import file, tools, client
 
 from ytmdl import const
 from ytmdl.const import LOCAL_SERVICE_SECRETS
 from ytmdl.utils.io_utils import safe_open
 
-YOUTUBE_SCOPES = ["https://www.googleapis.com/auth/youtube.readonly"]
-DRIVE_SCOPES = ["https://www.googleapis.com/auth/drive.file"]
+SCOPE_MAP = {
+    "youtube": ["https://www.googleapis.com/auth/youtube.readonly"],
+    "drive": ["https://www.googleapis.com/auth/drive.file"]
+}
 
 
 def is_cloud():
@@ -26,8 +28,12 @@ def get_service_creds():
         return credentials.Certificate(LOCAL_SERVICE_SECRETS)
 
 
+def _get_token_path(service_name, service_version):
+    return os.path.join(const.TMP_PATH, service_name + service_version + "token.json")
+
+
 def get_service_object(service_name, service_version, token):
-    token_path = os.path.join(const.TMP_PATH, "token.json")
+    token_path = _get_token_path(service_name, service_version)
     store = file.Storage(token_path)
     type_match_success = True
     with safe_open(token_path, 'w') as fp:
@@ -43,22 +49,13 @@ def get_service_object(service_name, service_version, token):
 
     return googleapiclient.discovery.build(service_name, service_version, credentials=store.get())
 
-# TODO: registration
-# def _lazy_init_clint_secrets(secrets_path):
-#     if not os.path.exists(secrets_path):
-#         config_dict_ref = firestore.find(const.FIRESTORE_APP, const.FIRESTORE_APP_CONFIG)
-#         config_dict = firestore.to_dict(config_dict_ref)
-#         client_secrets_str = config_dict[const.FIRESTORE_APP_CONFIG_KEY_CLIENT_SECRETS]
-#         with safe_open(secrets_path, 'w') as fp:
-#             fp.write(client_secrets_str)
-# def authenticate(service, service_version):
-#     secrets_path = '/tmp/ytmdl/client_secrets.json'
-#     _lazy_init_clint_secrets(secrets_path)
-#     flow = client.flow_from_clientsecrets(secrets_path, SCOPES)
-#     creds = tools.run_flow(flow, store)
-#
-#     with safe_open(token_path) as fp:
-#         token_str = fp.read()
-#         user_ref.set({const.FIRESTORE_USERS_KEY_TOKEN: token_str})
-#
-#     return googleapiclient.discovery.build(service, version, credentials=creds)
+
+def authenticate(service_name, service_version, secrets_path):
+    token_path = _get_token_path(service_name, service_version)
+    store = file.Storage(token_path)
+    flow = client.flow_from_clientsecrets(secrets_path, SCOPE_MAP[service_name.lower()])
+    creds = tools.run_flow(flow, store)
+
+    googleapiclient.discovery.build(service_name, service_version, credentials=creds)
+
+    return token_path
