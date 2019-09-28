03d8a1f2a5141ad0c33867cae463249875e9cce6
jaeseopark <20038316+jaeseopark@users.noreply.github.com>
2019-09-28 01:59:54 -0600

    added comments

--- ytmdl/firestore.py
+++ ytmdl/firestore.py
@@ -14,14 +14,20 @@ LOGGER = logging.getLogger(__name__)
 
 
 class FirestoreClient:
+    """
+    Singleton class to store the Google Firestore client. The init logic is encapsulated in this structure because
+    having the init logic in the global scope causes unit tests to fail.
+    """
     __instance = None
 
     def __init__(self):
         def lazy_init():
             creds = gcp_utils.get_service_creds()
             if gcp_utils.is_cloud():
+                # If runtime is Cloud Functions, then specify the project id.
                 firebase_admin.initialize_app(creds, {'projectId': os.getenv('GCP_PROJECT')})
             else:
+                # service_creds.json is required for all other environments and the file already contains the project id.
                 firebase_admin.initialize_app(creds)
 
             return firestore.client()
@@ -59,6 +65,11 @@ def _find(doc_ref, *args):
 
 
 def find(*args):
+    """
+    Given an arbitrary number of string arguments, returns the Firestore reference object.
+    :param args: string values
+    :return: A Firestore reference object that corresponds to the given 'args'
+    """
     return _find(FirestoreClient().instance, *args)
 
 
@@ -71,6 +82,17 @@ def to_dict(doc_ref):
 
 
 def handle_event(firestore_event: dict):
+    """
+    Converts the raw Firestore event into the following format:
+    {
+      "field1": [old_value, new_value],
+      "field2": [old_value, new_value],
+      ...
+    }
+    :param firestore_event: Original Firestore event
+    :return: The converted event
+    """
+
     def sanitize(field_value_dict):
         if field_value_dict is None:
             return None
