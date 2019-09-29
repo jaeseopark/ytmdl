784c2267073dca78c5ca9b16cdebaa8dcea27785
jaeseopark <20038316+jaeseopark@users.noreply.github.com>
2019-09-29 10:52:08 -0600

    adding mimetype=audio/mp4

--- test/manual/test_process_video.py
+++ test/manual/test_process_video.py
@@ -3,7 +3,7 @@ import json
 from ytmdl.process_video import process_video
 
 
-def test_process_video():
+def test_upload_video():
     with open('test/resource/firestore_event_video_create_new_doc.json') as fp:
         event = json.load(fp)
 
